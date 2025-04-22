37c4c1589099f04ebe663d4358f002918131b14a
jung2012pt <jung-pt@hotmail.com>
2025-04-23 06:12:32 +0700

    ni

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import About from "./pages/About";
 import Course from "./pages/Course";
 import "./App.css"; // Create a CSS file for styling
